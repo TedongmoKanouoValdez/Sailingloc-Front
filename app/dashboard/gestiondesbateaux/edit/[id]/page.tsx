6a7d0dfcba436fc056d1de0872702572d8e4fe1f
ArmadaHannibal <ynkoumate@gmail.com>
2025-08-07 16:05:34 +0200

    Fix syntax error in cancellation policies and Image/

--- app/dashboard/gestiondesbateaux/edit/[id]/page.tsx
+++ app/dashboard/gestiondesbateaux/edit/[id]/page.tsx
@@ -1297,7 +1297,7 @@ export default function EditBateauForm() {
                           <span>Soumission...</span>
                         </>
                       ) : ( */}
-                        'Soumettre'
+                        Soumettre
                         {/* )} */}
                       </button>
                     </div>
